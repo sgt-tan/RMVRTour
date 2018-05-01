17ddc9ebfe0bc017581e608797482a4ccf612adf
sgt-tan <stanleygtan@gmail.com>
2018-05-01 15:20:48 -0400

    Full Code

---
+++ Assets/Scripts/AudioPlay.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour {
+    public bool enter;
+    public AudioClip AudioClip;
+    private AudioSource source;
+    private bool played;
+
+    // Use this for initialization
+    void Start()
+    {
+        enter = false;
+        played = false;
+        source = GameObject.Find("FPSController").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && played == false)
+        {
+            enter = true;
+            played = true;
+            // Play the sound only on the trigger
+            source.Stop();
+            source.PlayOneShot(AudioClip);
+        }
+        Debug.Log("Entered");
+    }
+
+    //Deactivate the Main function when player is go away from AudioClip spot
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enter = false;
+            played = false;
+        }
+        Debug.Log("Exited");
+    }
+}
---
+++ Assets/Scripts/Marker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marker : MonoBehaviour {
+
+    public Transform player;
+    private TextMesh mesh;
+    private string text;
+    public bool enabled;
+
+    private void Start() {
+        mesh = GetComponent<TextMesh>();
+        text = mesh.text;
+    }
+
+    void Update() {
+        mesh.fontSize = (int) (5 * Mathf.Sqrt(Vector3.Distance(player.position, transform.position) + 1) + 10);
+        transform.LookAt(player);
+        if (enabled) {
+            Ray ray = new Ray(player.position, player.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100) && hit.transform.position == transform.position) {
+                mesh.text = text;
+            } else {
+                mesh.text = "ↂ";
+            }
+        } else {
+            mesh.text = "";
+        }
+    }
+
+}
